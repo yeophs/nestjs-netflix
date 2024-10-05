9e9991d32699c4e123785170b5ea362424c64073
yeeeeeeeop <yeop0916@gmail.com>
2024-10-05 22:14:40 +0900

    feat: whitelist, forbidNonWhitelisted 활성화

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 애초에 정의하지 않은 값들은 전달되지 않는다.
+      forbidNonWhitelisted: true, // 있으면 안되는 property를 금지한다.
+    }),
+  );
   await app.listen(3000);
 }
+
 bootstrap();
