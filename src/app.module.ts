c2821ace2ec998164f4a9d4e1aa8cc0571035d4e
yeeeeeeeop <yeop0916@gmail.com>
2024-10-07 23:15:31 +0900

    chore: 환경변수 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,48 @@
 import { Module } from '@nestjs/common';
 import { MoviesModule } from './movies/movies.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [MoviesModule], // 또 다른 모듈을 이 모듈로 불러들일 때 사용
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 어떤 모듈에서든 ConfigModule에 등록된 환경변수들을 사용할 수 있도록 해줌
+      validationSchema: Joi.object({
+        ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_TYPE: Joi.string().valid('postgres').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+      }),
+    }),
+    // 비동기여야 되는 이유? ConfigModule이 IoC에 모두 등록된 이후 받으려고!
+    TypeOrmModule.forRootAsync({
+      useFactory: (configService: ConfigService) => ({
+        type: configService.get<string>('DB_TYPE') as 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_DATABASE'),
+        entities: [],
+        synchronize: true, // 개발할 때만 true, prod에선 false
+      }),
+      inject: [ConfigService],
+    }),
+    // TypeOrmModule.forRoot({
+    //   type: process.env.DB_TYPE as 'postgres',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   entities: [],
+    //   synchronize: true, // 개발할 때만 true, prod에선 false
+    // }),
+    MoviesModule,
+  ], // 또 다른 모듈을 이 모듈로 불러들일 때 사용
 })
 export class AppModule {}
