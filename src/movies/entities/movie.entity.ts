49711682f54c4355b7e903d3e08a8dcdee4e70e7
yeeeeeeeop <yeop0916@gmail.com>
2024-10-08 13:46:40 +0900

    feat: BaseEntity 추가

--- src/movies/entities/movie.entity.ts
+++ src/movies/entities/movie.entity.ts
@@ -7,17 +7,7 @@ import {
   VersionColumn,
 } from 'typeorm';
 
-@Entity()
-export class Movie {
-  @PrimaryGeneratedColumn()
-  id: number;
-
-  @Column()
-  title: string;
-
-  @Column()
-  genre: string;
-
+export class BaseEntity {
   @CreateDateColumn()
   createdAt: Date;
 
@@ -26,6 +16,18 @@ export class Movie {
 
   @VersionColumn()
   version: number;
+}
+
+@Entity()
+export class Movie extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  genre: string;
 }
 
 /*
