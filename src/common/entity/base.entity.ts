61173bd691ac87640f146d8197e08af704b30838
yeeeeeeeop <yeop0916@gmail.com>
2024-10-11 21:14:01 +0900

    feat: class-transformer의 exclude 이용

--- src/common/entity/base.entity.ts
+++ src/common/entity/base.entity.ts
@@ -1,12 +1,16 @@
 import { CreateDateColumn, UpdateDateColumn, VersionColumn } from 'typeorm';
+import { Exclude } from 'class-transformer';
 
 export class BaseEntity {
   @CreateDateColumn()
+  @Exclude()
   createdAt: Date;
 
   @UpdateDateColumn()
+  @Exclude()
   updatedAt: Date;
 
   @VersionColumn()
+  @Exclude()
   version: number;
 }
