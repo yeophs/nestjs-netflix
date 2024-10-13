2b864786444044b1c872097d16d50d3e555fc1a1
yeeeeeeeop <yeop0916@gmail.com>
2024-10-13 16:52:01 +0900

    feat: 토큰 만료 예외처리 추가

--- src/auth/middleware/bearer-token.middleware.ts
+++ src/auth/middleware/bearer-token.middleware.ts
@@ -46,6 +46,10 @@ export class BearerTokenMiddleware implements NestMiddleware {
       req.user = payload;
       next();
     } catch (err) {
+      if (err.name === 'TokenExpiredError') {
+        throw new UnauthorizedException('토큰이 만료됐습니다.');
+      }
+
       next();
     }
   }
