8290249d833596635e034b4bd6a3659e72b3d983
yeeeeeeeop <yeop0916@gmail.com>
2024-10-17 15:22:10 +0900

    fix: page, take ? 삭제

--- src/common/dto/page-pagination.dto.ts
+++ src/common/dto/page-pagination.dto.ts
@@ -3,9 +3,9 @@ import { IsInt, IsOptional } from 'class-validator';
 export class PagePaginationDto {
   @IsInt()
   @IsOptional()
-  page?: number = 1;
+  page: number = 1;
 
   @IsInt()
   @IsOptional()
-  take?: number = 5;
+  take: number = 5;
 }
